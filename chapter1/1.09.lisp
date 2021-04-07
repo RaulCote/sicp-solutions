fc17b37a7830b4146eaa6058938d81c880fb4f89
Raul <raulcote@hotmail.com>
2021-04-07 08:42:10 +0200

    Added 1.09.

--- chapter1/1.09.lisp
+++ chapter1/1.09.lisp
@@ -28,13 +28,9 @@
 ;
 ; second procedure solution is iterative
 ; (+ 4 5)
-; (+ (dec 4) (inc 5))
 ; (+ 3 6)
-; (+ (dec 3) (inc 6))
 ; (+ 2 7)
-; (+ (dec 2) (inc 7))
 ; (+ 1 8)
-; (+ (dec 1) (inc 8))
 ; (+ 0 9)
 ; 9
 
